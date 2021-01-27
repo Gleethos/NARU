74822bfddb2c2de9a0766e2bfeee18c692c32411
Gleethos <danneydan@gmx.at>
2021-01-27 17:36:59 +0100

    quick "save"

--- utility/classes.py
+++ utility/classes.py
@@ -1,21 +1,20 @@
-import math
+
 from collections import defaultdict
 import threading
+import torch
 
 CONTEXT = threading.local()
 CONTEXT.recorders = []
 
-import torch
-
 torch.manual_seed(666)
 
-
+# Moments are responsible for holding variables needed for propagating back through time.
 class Moment:
     def __init__(self):
-        self.is_record = False
+        self.is_record = False # Moments can be records or "on the fly creations" provided by the defaultdict...
     pass
 
-
+# A useful method used later on...
 def sig(x, derive=False):
     s = torch.sigmoid(x)
     if derive:
@@ -28,7 +27,8 @@ def sig(x, derive=False):
 # HISTORY BASE CLASS
 
 class Recorder:
-    def __init__(self, default_lambda):
+    def __init__(self, default_lambda, progression_lambda=None):
+        self.progression_lambda = progression_lambda
         self.default_lambda = default_lambda
         self.history: dict = None  # history
         self.reset()
@@ -38,7 +38,8 @@ class Recorder:
         self.history: dict = defaultdict(self.default_lambda)  # history
 
     def at(self, time: int):
-        is_record = self.history[time].is_record
+        moment = self.history[time]
+        is_record = moment.is_record
         while not is_record:
             time -= 1
             is_record = self.history[time].is_record
@@ -46,6 +47,8 @@ class Recorder:
                 self.history[0] = self.history[0]
                 self.history[0].is_recorded = True
                 return self.history[0]
+        if self.progression_lambda is not None:
+            return self.progression_lambda(self.history[time],moment)
         return self.history[time]
 
     def rec(self, time: int):
@@ -54,7 +57,7 @@ class Recorder:
         return self.history[time]
 
 
-# ----------------------------------------------------------------------------------------------------------------------#
+# -------------------------------------------------------------------------------------------------------------------- #
 # ROUTE
 
 class Route(Recorder):
@@ -93,12 +96,16 @@ class Route(Recorder):
         return (g_h, g_r)
 
 
-
+print('Route loaded! Unit-Testing now...')
 # TESTING:
 
 torch.manual_seed(66642999)
 
 route = Route(D_in=3, D_out=2)
+
+assert len(CONTEXT.recorders) == 1
+assert CONTEXT.recorders[0] == route
+
 r = torch.ones(1, 3)
 h = torch.ones(1, 2)
 
@@ -118,14 +125,17 @@ assert str(route.Wgr.grad) == "tensor([[ 0.1344],\n        [-0.5378],\n        [
 assert str(route.Wr.grad) == "tensor([[ 0.3515, -0.3515],\n        [-1.4062,  1.4062],\n        [-0.7031,  0.7031]])"
 
 del route, r, h, c, g_r, g_h
-
+CONTEXT.recorders = []
+print('Route Unit-Testing successful!')
 
 # ----------------------------------------------------------------------------------------------------------------------#
 # SOURCE
 
+
 def default_source_moment():
     m = Moment()
     m.g = -1
+
 
 class Source(Recorder):
     def __init__(self, D_in: int, D_out: int):
@@ -143,12 +153,16 @@ class Source(Recorder):
         return e_s.matmul(self.Ws.T)
 
 
-
 # TESTING:
+print('Source loaded! Unit-Testing now...')
 
 torch.manual_seed(66642999)
 
 source = Source(D_in=2, D_out=3)
+
+assert len(CONTEXT.recorders) == 1
+assert CONTEXT.recorders[0] == source
+
 s = torch.ones(1, 2)
 c = source.forward(s)
 assert str(c) == 'tensor([[-2.4602,  1.0154,  1.8009]])' # shape=(1,3)
@@ -159,7 +173,8 @@ assert str(e_c) == 'tensor([[-0.3409,  0.6971]])' # shape=(1,2)
 assert str(source.Ws.grad) == "tensor([[ 2.,  2.,  2.],\n        [-1., -1., -1.]])"
 
 del s, c, e, e_c
-
+CONTEXT.recorders = []
+print('Source Unit-Testing successful!')
 
 # ----------------------------------------------------------------------------------------------------------------------#
 # GROUP
@@ -171,23 +186,43 @@ def default_moment(dimensionality: int):
     m.state = torch.zeros(1, dimensionality)
     m.is_sleeping = True
     m.derivative = None
+    m.error = None
+    m.error_count = 0
     return m
+
+# The Recorder instance will find the most recent record wehn calling the "at" method...
+# However some properties might not be reflective of the true current (non record) moment!
+def default_progression(older : Moment, moment : Moment):
+    new = Moment()
+    new.state = older.state
+    new.derivative = older.derivative
+    new.is_sleeping = moment.is_sleeping
+    new.error = older.error
+    new.error_count = older.error_count
+    return new
 
 
 class Group(Recorder):
 
     def __init__(self, index: int, dimensionality: int):
-        super().__init__(default_lambda=lambda: default_moment(dimensionality))
+        super().__init__(default_lambda=lambda: default_moment(dimensionality), progression_lambda=default_progression)
         self.from_conns = dict()
         self.to_conns = dict()
         self.index = index
         self.dimensionality = dimensionality
 
         self.targets = []  # indices of targeted routes! (routes to groups)
-        self.error = None  # the current accumulated error
-        self.error_count = 0  # the number of errors summed up within the current accumulated error
+        #self.error = None  # the current accumulated error
+        #self.error_count = 0  # the number of errors summed up within the current accumulated error
 
     # IO :
+
+    def add_error(self, e, time):
+        if self.at(time).error is None:
+            self.rec(time).error = e
+        else:
+            self.rec(time).error = self.at(time).error + e
+        self.rec(time).error_count = self.at(time) + 1 #-> incrementing the counter! (for normalization)
 
     def get_params(self):
         params = []
@@ -209,17 +244,18 @@ class Group(Recorder):
 
     def connect_forward(self, next_groups: list, cone_size: int, step: int):
         assert step > 0
+        next_groups = [g for g in next_groups if g not in self.from_conns and g not in self.to_conns]
         cone_size = min(cone_size, len(next_groups))
         for i in range(cone_size):
             target_index = (self.index + i * step) % len(next_groups)
             target_group = next_groups[target_index]
             self.targets.append(target_index)
             assert target_group.index == target_index
-            self.to_conns[target_group] = Route(self.dimensionality, target_group.dimensionality)
+            self.to_conns[target_group] = Route(D_in=target_group.dimensionality, D_out=self.dimensionality)
             target_group.register_source(self)
 
     def register_source(self, origin_group):
-        self.from_conns[origin_group] = Source(origin_group.dimensionality, self.dimensionality)
+        self.from_conns[origin_group] = Source(D_in=origin_group.dimensionality, D_out=self.dimensionality)
 
     # Execution :
 
@@ -298,12 +334,7 @@ class Group(Recorder):
                     e_s=current_error,
                     s=group.at(time).state  # Needed to calculate gradients of the weights!
                 )
-
-                if group.error is None:
-                    group.error = g_s  # setting or accumulating the error!
-                else:
-                    group.error = group.error + g_s
-                group.error_count += 1 #-> incrementing the counter! (for normalization)
+                group.add_error(g_s) # setting or accumulating the error!
 
             # Route (error) back-prop :
             for group, route in self.to_conns.items():  # Distributing error to route groups...
@@ -314,19 +345,10 @@ class Group(Recorder):
                     r=route.at(time).state,  # Needed to calculate gradients of the weights!
                     h=self.at(time).state  # Needed to calculate gradients of the weights!
                 )
-
-                if group.error is None:
-                    group.error = g_r  # setting or accumulating the error!
-                else:
-                    group.error = group.error + g_r
-                group.error_count += 1 #-> incrementing the counter! (for normalization)
+                group.add_error(g_r)
 
                 # Accumulating g_h to self:
-                if self.error is None:
-                    self.error = g_h
-                else:
-                    self.error = self.error + g_h
-                self.error_count += 1 #-> incrementing the counter! (for normalization)
+                self.add_error(g_h)
 
             # Source Group backprop :
             for group, source in self.from_conns.items():
@@ -338,15 +360,69 @@ class Group(Recorder):
 
     def activation(self, x, derive=False):  # State of the Art activation function, SWISH
         if derive:
-            return torch.sigmoid(x)
+            return sig(x)
         else:
-            return torch.sigmoid(x) * x
+            return sig(x) * x
         # return x * torch.tanh(nn.functional.softplus(x)) # MISH might also be interesting
 
 
+print('Group loaded! Unit-Testing now...')
 # TESTING:
 
 torch.manual_seed(66642999)
+
+
+def test_simple_net(group, other1, other2, output):
+    # connecting them...
+    group.connect_forward(next_groups=[other1,other2], cone_size=293943, step=1)
+    other1.connect_forward(next_groups=[output], cone_size=123, step=1)
+    other2.connect_forward(next_groups=[output], cone_size=123, step=1)
+
+    assert len(group.from_conns) == 0
+    assert len(group.to_conns) == 2
+    assert len(other1.from_conns) == 1
+    assert len(other2.from_conns) == 1
+    assert len(other1.to_conns) == 1
+    assert len(other2.to_conns) == 1
+    assert len(output.from_conns) == 2
+    assert len(output.to_conns) == 0
+    assert len(CONTEXT.recorders) == 4 + 2 + 2 + 2 + 2# four groups and 6 connections
+
+    group.start_with(torch.tensor([[1.0, 2.0, 3.0]]))
+    groups = [group, other1, other2, output]
+    for g in groups: g.forward(0)
+
+    assert not other1.at(1).is_sleeping # CHOICE: other1
+    assert other2.at(1).is_sleeping
+    assert output.at(1).is_sleeping
+    assert [r.at(0).g for r in group.to_conns.values()] == [0.10948651283979416, 0.0009388707112520933]
+
+    # Future states don't know anything:
+    assert other1.at(2).is_sleeping
+    assert other2.at(2).is_sleeping
+    assert output.at(2).is_sleeping
+
+    group.start_with(torch.tensor([[-3.0, -1.0, -4.0]]))
+    for g in groups: g.forward(1)
+
+    assert not other1.at(1).is_sleeping # first step is still recorded...
+    assert other2.at(1).is_sleeping
+    assert [r.at(0).g for r in group.to_conns.values()] == [0.10948651283979416, 0.0009388707112520933]
+
+    assert other1.at(2).is_sleeping # New step as well!
+    assert not other2.at(2).is_sleeping # CHOICE: other2
+    assert not output.at(2).is_sleeping
+    assert [r.at(1).g for r in group.to_conns.values()] == [0.881648063659668, 0.9996621608734131]
+    assert [r.at(1).g for r in other1.to_conns.values()] == [0.5]
+
+    #last activation (activates output)
+    for g in groups: g.forward(2)
+    for g in groups: assert g.at(0).error_count == 0
+
+    #output.add_error(torch.tensor([[1]]))
+    #assert output.error_count == 1
+
+
 
 group = Group(index=0, dimensionality=3)
 
@@ -355,221 +431,20 @@ assert group.dimensionality == 3
 assert group.from_conns != None
 assert group.to_conns != None
 assert group.targets == []  # indices of targeted routes!
-assert group.error == None
-assert group.error_count == 0
+assert group.at(0).error == None
+assert group.at(0).error_count == 0
 assert group.at != None
 
 other1 = Group(index=0,dimensionality=3)
 other2 = Group(index=1,dimensionality=3)
+output = Group(index=0,dimensionality=1)
 
-# connecting them...
-group.connect_forward(next_groups=[other1,other2], cone_size=293943, step=1)
+test_simple_net(group, other1, other2, output)
+for r in CONTEXT.recorders: r.reset() # Resetting allows for a repeat of the test!
+test_simple_net(group, other1, other2, output)
 
-assert len(group.from_conns) == 0
-assert len(group.to_conns) == 2
-assert len(other1.from_conns) == 1
-assert len(other2.from_conns) == 1
-assert len(other1.to_conns) == 0
-assert len(other2.to_conns) == 0
+del group, other1, other2, output
 
-group.start_with(torch.tensor([[1.0, 2.0, 3.0]]))
-groups = [group, other1, other2]
-for g in groups: g.forward(0)
-
-assert not other1.at(1).is_sleeping
-assert other2.at(1).is_sleeping
-assert [r.at(0).g for r in group.to_conns.values()] == [0.10948651283979416, 0.0009388707112520933]
-
-for g in groups: g.forward(1)
-
-del group, other1, other2
-
-
-# ----------------------------------------------------------------------------------------------------------------------#
-# CAPSULE
-
-class Capsule:
-
-    def __init__(self, dimensionality, size: int):
-        self.groups = []
-        for i in range(size):  # creating "size" number of groups for this capsule
-            self.groups.append(Group(i, dimensionality))
-
-    # IO :
-
-    def get_params(self):
-        params = []
-        for group in self.groups:
-            params.extend(group.get_params())
-        return params
-
-    def str(self, level):
-        asStr = level + 'Capsule: {\n'
-        asStr += (level + '   height: ' + str(len(self.groups)) + '\n')
-        for group in self.groups:
-            asStr += group.str(level + '   ')
-        asStr += ('\n' + level + '};\n')
-        return asStr
-
-    # Construction :
-
-    def connect_forward(self, next_capsule, max_cone, step):
-        for group in self.groups:
-            group.connect_forward(next_capsule.groups, max_cone, step)
-
-    # Execution :
-
-    def start_with(self, x):
-        # This is only allowed to be called
-        # on first capsule of the network !
-        assert len(self.groups) == 1
-        for group in self.groups: group.start_with(x)
-
-
-# ----------------------------------------------------------------------------------------------------------------------#
-# Network
-
-class Network:
-
-    def __init__(self,
-                 depth=5,
-                 max_height=15,
-                 max_dim=500,
-                 max_cone=39,
-                 D_in=100,
-                 D_out=10,
-                 ):
-        assert depth > 3
-        dims = [math.floor(float(x)) for x in self.girth_from(depth, D_in, max_dim, D_out)]
-        heights = [math.floor(float(x)) for x in self.girth_from(depth, 1, max_height, 1)]
-        self._capsules = []
-
-        for i in range(depth):
-            self._capsules.append(Capsule(dims[i], heights[i]))
-
-        for i in range(depth):
-            if i != depth - 1:
-                assert len(self._capsules[i].groups) * max_cone >= len(self._capsules[i + 1].groups)
-                current_cone = min(len(self._capsules[i + 1].groups), max_cone)
-                step = max(1, int(len(self._capsules[i + 1].groups) / current_cone))
-                self._capsules[i].connect_forward(self._capsules[i + 1], current_cone, step)
-
-    def str(self):
-        asStr = ''
-        asStr += 'Network: {\n'
-        asStr += '   length: '
-        asStr += str(len(self._capsules)) + '\n'
-        level = '   '
-        for capsule in self._capsules:
-            asStr += capsule.str(level)
-        asStr += '\n};\n'
-        return asStr
-
-    def girth_from(self, length, start, max, end):
-        g, mid = [], (length - 1) / 2
-        for i in range(length):
-            if math.fabs(mid - i) < 1:
-                g.append(max)
-            else:
-                if i < mid:
-                    ratio = i / mid
-                    g.append((max * ratio) + start * (1 - ratio))
-                else:
-                    ratio = (i - mid) / mid
-                    g.append((end * ratio) + max * (1 - ratio))
-        return g
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# TESTING :
-
-torch.manual_seed(66642999)
-
-net = Network(
-    depth=5,
-    max_height=15,
-    max_dim=500,
-    max_cone=8,
-    D_in=100,
-    D_out=10,
-)
-# print(net.str())
-
-expected_structure = [
-    {
-        'height': 1, 'dimensionality': 100, 'from': 0, 'to': 8,
-        'targets': [[0, 1, 2, 3, 4, 5, 6, 7]],
-        'index': [0]
-    },
-    {
-        'height': 8, 'dimensionality': 300, 'from': 1, 'to': 8,
-        'targets': [
-            [0, 1, 2, 3, 4, 5, 6, 7],
-            [1, 2, 3, 4, 5, 6, 7, 8],
-            [2, 3, 4, 5, 6, 7, 8, 9],
-            [3, 4, 5, 6, 7, 8, 9, 10],
-            [4, 5, 6, 7, 8, 9, 10, 11],
-            [5, 6, 7, 8, 9, 10, 11, 12],
-            [6, 7, 8, 9, 10, 11, 12, 13],
-            [7, 8, 9, 10, 11, 12, 13, 14],
-        ],
-        'index': [0, 1, 2, 3, 4, 5, 6, 7]
-    },
-    {
-        'height': 15, 'dimensionality': 500, 'from': [1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1],
-        'to': 8,
-        'targets': [
-            [0, 1, 2, 3, 4, 5, 6, 7],
-            [1, 2, 3, 4, 5, 6, 7, 0],
-            [2, 3, 4, 5, 6, 7, 0, 1],
-            [3, 4, 5, 6, 7, 0, 1, 2],
-            [4, 5, 6, 7, 0, 1, 2, 3],
-            [5, 6, 7, 0, 1, 2, 3, 4],
-            [6, 7, 0, 1, 2, 3, 4, 5],
-            [7, 0, 1, 2, 3, 4, 5, 6],
-            [0, 1, 2, 3, 4, 5, 6, 7],
-            [1, 2, 3, 4, 5, 6, 7, 0],
-            [2, 3, 4, 5, 6, 7, 0, 1],
-            [3, 4, 5, 6, 7, 0, 1, 2],
-            [4, 5, 6, 7, 0, 1, 2, 3],
-            [5, 6, 7, 0, 1, 2, 3, 4],
-            [6, 7, 0, 1, 2, 3, 4, 5]
-        ],
-        'index': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    },
-    {
-        'height': 8, 'dimensionality': 255, 'from': 15, 'to': 1,
-        'targets': [[0], [0], [0], [0], [0], [0], [0], [0]],
-        'index': [0]
-    },
-    {
-        'height': 1, 'dimensionality': 10, 'from': 8, 'to': 0,
-        'targets': [[]],
-        'index': [0]
-    }
-]
-
-assert len(net._capsules) == len(expected_structure)
-
-for ci in range(len(net._capsules)):
-    expected_capsule = expected_structure[ci]
-    given_capsule = net._capsules[ci]
-    assert expected_capsule['height'] == len(given_capsule.groups)
-    for gi in range(len(given_capsule.groups)):
-        given_group = given_capsule.groups[gi]
-        assert given_group.targets == expected_capsule['targets'][gi]
-        if isinstance(expected_capsule['from'], list):
-            assert len(given_group.from_conns) == expected_capsule['from'][gi]
-        else:
-            assert len(given_group.from_conns) == expected_capsule['from']
-        assert len(given_group.to_conns) == expected_capsule['to']
-
-net = Network(
-    depth=4,
-    max_height=3,
-    max_dim=500,
-    max_cone=8,
-    D_in=100,
-    D_out=10,
-)
-# print(net.str())
+CONTEXT.recorders = []
+print('Group Unit-Testing successful!')
+print('==============================\n')
---
+++ utility/ffnaru.py
@@ -0,0 +1,193 @@
+
+from utility.classes import Group
+import math
+import torch
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# CAPSULE
+
+
+class Capsule:
+
+    def __init__(self, dimensionality, size: int):
+        self.groups = []
+        for i in range(size):  # creating "size" number of groups for this capsule
+            self.groups.append(Group(i, dimensionality))
+
+    # IO :
+
+    def get_params(self):
+        params = []
+        for group in self.groups:
+            params.extend(group.get_params())
+        return params
+
+    def str(self, level):
+        asStr = level + 'Capsule: {\n'
+        asStr += (level + '   height: ' + str(len(self.groups)) + '\n')
+        for group in self.groups:
+            asStr += group.str(level + '   ')
+        asStr += ('\n' + level + '};\n')
+        return asStr
+
+    # Construction :
+
+    def connect_forward(self, next_capsule, max_cone, step):
+        for group in self.groups:
+            group.connect_forward(next_capsule.groups, max_cone, step)
+
+    # Execution :
+
+    def start_with(self, x):
+        # This is only allowed to be called
+        # on first capsule of the network !
+        assert len(self.groups) == 1
+        for group in self.groups: group.start_with(x)
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Network
+
+class Network:
+
+    def __init__(self,
+                 depth=5,
+                 max_height=15,
+                 max_dim=500,
+                 max_cone=39,
+                 D_in=100,
+                 D_out=10,
+                 ):
+        assert depth > 3
+        dims = [math.floor(float(x)) for x in self.girth_from(depth, D_in, max_dim, D_out)]
+        heights = [math.floor(float(x)) for x in self.girth_from(depth, 1, max_height, 1)]
+        self._capsules = []
+
+        for i in range(depth):
+            self._capsules.append(Capsule(dims[i], heights[i]))
+
+        for i in range(depth):
+            if i != depth - 1:
+                assert len(self._capsules[i].groups) * max_cone >= len(self._capsules[i + 1].groups)
+                current_cone = min(len(self._capsules[i + 1].groups), max_cone)
+                step = max(1, int(len(self._capsules[i + 1].groups) / current_cone))
+                self._capsules[i].connect_forward(self._capsules[i + 1], current_cone, step)
+
+    def str(self):
+        asStr = ''
+        asStr += 'Network: {\n'
+        asStr += '   length: '
+        asStr += str(len(self._capsules)) + '\n'
+        level = '   '
+        for capsule in self._capsules:
+            asStr += capsule.str(level)
+        asStr += '\n};\n'
+        return asStr
+
+    def girth_from(self, length, start, max, end):
+        g, mid = [], (length - 1) / 2
+        for i in range(length):
+            if math.fabs(mid - i) < 1:
+                g.append(max)
+            else:
+                if i < mid:
+                    ratio = i / mid
+                    g.append((max * ratio) + start * (1 - ratio))
+                else:
+                    ratio = (i - mid) / mid
+                    g.append((end * ratio) + max * (1 - ratio))
+        return g
+
+
+# TESTING :
+
+torch.manual_seed(66642999)
+
+net = Network(
+    depth=5,
+    max_height=15,
+    max_dim=500,
+    max_cone=8,
+    D_in=100,
+    D_out=10,
+)
+# print(net.str())
+
+expected_structure = [
+    {
+        'height': 1, 'dimensionality': 100, 'from': 0, 'to': 8,
+        'targets': [[0, 1, 2, 3, 4, 5, 6, 7]],
+        'index': [0]
+    },
+    {
+        'height': 8, 'dimensionality': 300, 'from': 1, 'to': 8,
+        'targets': [
+            [0, 1, 2, 3, 4, 5, 6, 7],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            [2, 3, 4, 5, 6, 7, 8, 9],
+            [3, 4, 5, 6, 7, 8, 9, 10],
+            [4, 5, 6, 7, 8, 9, 10, 11],
+            [5, 6, 7, 8, 9, 10, 11, 12],
+            [6, 7, 8, 9, 10, 11, 12, 13],
+            [7, 8, 9, 10, 11, 12, 13, 14],
+        ],
+        'index': [0, 1, 2, 3, 4, 5, 6, 7]
+    },
+    {
+        'height': 15, 'dimensionality': 500, 'from': [1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1],
+        'to': 8,
+        'targets': [
+            [0, 1, 2, 3, 4, 5, 6, 7],
+            [1, 2, 3, 4, 5, 6, 7, 0],
+            [2, 3, 4, 5, 6, 7, 0, 1],
+            [3, 4, 5, 6, 7, 0, 1, 2],
+            [4, 5, 6, 7, 0, 1, 2, 3],
+            [5, 6, 7, 0, 1, 2, 3, 4],
+            [6, 7, 0, 1, 2, 3, 4, 5],
+            [7, 0, 1, 2, 3, 4, 5, 6],
+            [0, 1, 2, 3, 4, 5, 6, 7],
+            [1, 2, 3, 4, 5, 6, 7, 0],
+            [2, 3, 4, 5, 6, 7, 0, 1],
+            [3, 4, 5, 6, 7, 0, 1, 2],
+            [4, 5, 6, 7, 0, 1, 2, 3],
+            [5, 6, 7, 0, 1, 2, 3, 4],
+            [6, 7, 0, 1, 2, 3, 4, 5]
+        ],
+        'index': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    },
+    {
+        'height': 8, 'dimensionality': 255, 'from': 15, 'to': 1,
+        'targets': [[0], [0], [0], [0], [0], [0], [0], [0]],
+        'index': [0]
+    },
+    {
+        'height': 1, 'dimensionality': 10, 'from': 8, 'to': 0,
+        'targets': [[]],
+        'index': [0]
+    }
+]
+
+assert len(net._capsules) == len(expected_structure)
+
+for ci in range(len(net._capsules)):
+    expected_capsule = expected_structure[ci]
+    given_capsule = net._capsules[ci]
+    assert expected_capsule['height'] == len(given_capsule.groups)
+    for gi in range(len(given_capsule.groups)):
+        given_group = given_capsule.groups[gi]
+        assert given_group.targets == expected_capsule['targets'][gi]
+        if isinstance(expected_capsule['from'], list):
+            assert len(given_group.from_conns) == expected_capsule['from'][gi]
+        else:
+            assert len(given_group.from_conns) == expected_capsule['from']
+        assert len(given_group.to_conns) == expected_capsule['to']
+
+net = Network(
+    depth=4,
+    max_height=3,
+    max_dim=500,
+    max_cone=8,
+    D_in=100,
+    D_out=10,
+)
+# print(net.str())
